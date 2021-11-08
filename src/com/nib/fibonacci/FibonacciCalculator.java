da41044be90ee100d4612a452ac2b3dd60fa1242
nixfuerdiecharts <nibra@posteo.de>
2021-11-08 15:36:05 +0100

    implemented recursive calculation for fibonacci

--- src/com/nib/fibonacci/FibonacciCalculator.java
+++ src/com/nib/fibonacci/FibonacciCalculator.java
@@ -1,5 +1,34 @@
 package com.nib.fibonacci;
 
-public class FibonacciCalculator {
+import java.math.BigInteger;
 
+/**
+ * Calculator class for nth fibonacci number with methods with different calculation modes
+ *
+ * @author Niko
+ */
+public class FibonacciCalculator {
+    /**
+     * Calculation for nth fibonacci number with recursion
+     *
+     * @param nthFibNumber nth number to calculate the corresponding fibonacci number
+     * @return Corresponding fibonacci number for the given parameter
+     */
+    private BigInteger fibonacciRecursive(int nthFibNumber) {
+        BigInteger result;
+
+        //Determine whether the recursion abortion is reached or not
+        if (nthFibNumber == 0) {
+            result = BigInteger.ZERO;
+        } else if (nthFibNumber == 1) {
+            result = BigInteger.ONE;
+        } else if (nthFibNumber >= 2) {
+            result = fibonacciRecursive(nthFibNumber - 1).add(fibonacciRecursive(nthFibNumber - 2));
+        } else {
+            //Given argument is illegal for fibonacci algorithm
+            throw new IllegalArgumentException("Illegal Argument in recursive fibonacci calculation. Argument can only be 0 or bigger");
+        }
+
+        return result;
+    }
 }
